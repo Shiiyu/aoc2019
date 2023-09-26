1923615681365fce63197c40552329c26241e343
Shiiyu <40838999+Shiiyu@users.noreply.github.com>
2023-09-25 21:26:24 -0400

    feat: :sparkles: day01b

---
+++ day01b/index.js
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+
+function totalFuel(mass) {
+  const fuel = Math.floor(mass / 3) - 2;
+
+  return fuel > 0
+    ? fuel + totalFuel(fuel)
+    : 0;
+}
+
+fs.readFile("./input.txt", "utf8", (_, data) => {
+  console.log(data.split("\n").map(n => totalFuel(Number(n))).reduce((a, b) => a + b));
+});
